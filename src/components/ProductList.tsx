fb58f0654f043200938d928d3fb3badc4ca37b14
Franco <franco.silva@mylo.de>
2024-08-28 12:47:07 +0100

    Create ProductList.tsx

---
+++ src/components/ProductList.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Product } from '../models/product';
+import ProductItem from './ProductItem';
+
+interface ProductListProps {
+  products: Product[];
+}
+
+const ProductList: React.FC<ProductListProps> = ({ products }) => {
+  return (
+    <div className="product-list">
+      <h2>Product List</h2>
+      {products.map((product) => (
+        <ProductItem key={product.id} product={product} />
+      ))}
+    </div>
+  );
+};
+
+export default ProductList;

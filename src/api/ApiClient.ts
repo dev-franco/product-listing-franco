37e4f2fb075c41cef216bd6d6307264edcdc55e6
Franco <franco.silva@mylo.de>
2024-08-28 15:27:59 +0100

    add getMetadata method to return categories

--- src/api/ApiClient.ts
+++ src/api/ApiClient.ts
@@ -1,9 +1,18 @@
 import data from '../api/products.json';
+import { Metadata } from '../models/metadata';
 
 export class ApiClient {
 
     public async getAll<T>(): Promise<T[]> {
         await new Promise(resolve => setTimeout(resolve, 500));
         return data as T[];
+    }
+
+    public async getMetadata(): Promise<Metadata> {
+        await new Promise(resolve => setTimeout(resolve, 250));
+        const metadata = new Metadata();
+        metadata.productCategories = [...new Set(data.map(d => d.category))].sort();
+
+        return metadata
     }
 }

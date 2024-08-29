0e8b3ecec843b8cc05bde6594aff54cc7523e3c9
Franco <franco.silva@mylo.de>
2024-08-29 12:21:08 +0100

    continue styling on products grid

--- src/components/ProductItem/ProductItem.tsx
+++ src/components/ProductItem/ProductItem.tsx
@@ -12,15 +12,16 @@ const ProductItem: React.FC<ProductItemProps> = ({ product }) => {
   return (
     <div className="ww-product-item">
       <Card
-        hoverable
         cover={<img alt={product.name} src={`https://random-image-pepebigotes.vercel.app/api/random-image?${product.id}`} />}
       >
-
-        <div className="ww-product-description">
-          <div className="ww-product-item-name"><span>{product.name}</span></div>
-          <div className="ww-product-item-category">{product.category}</div>
+        <div className="ww-product-meta">
+          <div className="ww-product-description">
+            <div className="ww-product-name"><span>{product.name}</span></div>
+            <div className="ww-product-category">{product.category}</div>
+          </div>
+          <div className="ww-product-price"><span>{product?.price?.toFixed(2)}€</span></div>
         </div>
-        <div className="ww-product-item-price">{product?.price?.toFixed(2)}€</div>
+        
       </Card>
     </div>
 

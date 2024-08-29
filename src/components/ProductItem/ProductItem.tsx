d5b7418d6d1147db570b03325d9bd35bfa781822
Franco <franco.silva@mylo.de>
2024-08-29 11:49:23 +0100

    continue styling - add grid to md()

--- src/components/ProductItem/ProductItem.tsx
+++ src/components/ProductItem/ProductItem.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Product } from '../../models/product';
-import './ProductItem.css';
+import './ProductItem.scss';
+import { Card } from 'antd';
 
 interface ProductItemProps {
   product: Product;
@@ -10,10 +11,20 @@ interface ProductItemProps {
 const ProductItem: React.FC<ProductItemProps> = ({ product }) => {
   return (
     <div className="ww-product-item">
-      <div className="ww-product-item-name"><span>{product.name}</span></div>
-      <div className="ww-product-item-category">Category: {product.category}</div>
-      <div className="ww-product-item-price">Price: {product?.price?.toFixed(2)}€</div>
+      <Card
+        hoverable
+        cover={<img alt={product.name} src={`https://random-image-pepebigotes.vercel.app/api/random-image?${product.id}`} />}
+      >
+
+        <div className="ww-product-description">
+          <div className="ww-product-item-name"><span>{product.name}</span></div>
+          <div className="ww-product-item-category">{product.category}</div>
+        </div>
+        <div className="ww-product-item-price">{product?.price?.toFixed(2)}€</div>
+      </Card>
     </div>
+
+
   );
 };
 
--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Product } from '../../models/product';
 import ProductItem from '../ProductItem/ProductItem';
-
+import './ProductList.scss';
 interface ProductListProps {
   products: Product[];
 }
 
 const ProductList: React.FC<ProductListProps> = ({ products }) => {
   return (
-    <div className="product-list">
+    <div className="ww-products-list-wrapper">
       {products.map((product) => (
         <ProductItem key={product.id} product={product} />
       ))}

74e9cdcf97680c76a49e8e3051b92c388edc07c6
Franco <franco.silva@mylo.de>
2024-08-29 11:21:37 +0100

    continue styling

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -17,7 +17,7 @@ const Filter: React.FC<FilterProps> = ({ categories, onCategoryChange }) => {
             { categories && ( categories.length ?? 0 ) > 0 && (
                 <Select onChange={e => handleFilterCategoryChange(e)} placeholder="Select a category">
                     <Select.Option value="">All categories</Select.Option>
-                {categories.map(category => (
+                    {categories.map(category => (
                     <Select.Option key={category} value={category}>{category}</Select.Option>
                 ))}
                 </Select>
